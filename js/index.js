49823e0b2a6234aeb739d92941644186112ec4bb
ama-ashar <ama.ashar@gmail.com>
2025-10-20 18:48:09 +0800

    Update splash navigation to new HTML path

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const SPLASH_DURATION = 2200;
 function hideSplashAndNavigate() {
     const splash = document.getElementById('splash');
     if (!splash) {
-        window.location.href = 'login.html';
+        window.location.href = 'html/login.html';
         return;
     }
 
@@ -18,7 +18,7 @@ function hideSplashAndNavigate() {
     setTimeout(() => {
         // remove splash from DOM after fade
         if (splash.parentNode) splash.parentNode.removeChild(splash);
-        window.location.href = 'login.html';
+        window.location.href = 'html/login.html';
     }, 450);
 }
 
@@ -27,4 +27,5 @@ if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', () => setTimeout(hideSplashAndNavigate, SPLASH_DURATION));
 } else {
     setTimeout(hideSplashAndNavigate, SPLASH_DURATION);
+
 }
